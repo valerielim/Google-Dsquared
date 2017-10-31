197602659f41f3037393f7c4e44899201d974149
Valerie Lim <limhy.valerie@gmail.com>
2017-10-31 10:27:54 +0800

    Update Reddit_API_Script.py

--- Python Projects/Reddit_API_Script.py
+++ Python Projects/Reddit_API_Script.py
@@ -25,7 +25,7 @@ def get_date(submission):
     return datetime.datetime.fromtimestamp(time)
     # Source: https://www.reddit.com/r/learnprogramming/comments/37kr5n/praw_is_it_possible_to_get_post_time_and_date/
 
-# Get comments for a reddit post, or list of reddit posts, as identified by each posts' ID number
+
 def get_comments(ID_list):
 
     # Empty df to hold comments
@@ -61,5 +61,46 @@ def get_comments(ID_list):
 ID_list = ['1gsdj9', '70s99c', '358af2']
 print get_comments(ID_list)
 
+# -------------------------------------------------------------------- #
+# Alt method for auth
+
+# Auth
+client_auth = requests.auth.HTTPBasicAuth(Client_ID, Client_secret)
+post_data = {"grant_type": "password",
+             "username": Username,
+             "password": Password}
+headers = {"User-Agent": "ChangeMeClient/5.2.0 by YourUsername"}
+response = requests.post("https://www.reddit.com/api/v1/access_token",
+                         auth=client_auth, data=post_data, headers=headers)
+
+# API Key
+a = response.json()
+key = a['access_token']
+
+# Info about me
+headers = {"Authorization": "bearer %s" % (key),
+           "User-Agent": "ChangeMeClient/0.1 by YourUsername"}
+response = requests.get("https://oauth.reddit.com/api/v1/me",
+                        headers=headers); pp.pprint(response.json())
+Get comments from one post from a subreddit
+response = requests.get("https://oauth.reddit.com/r/askreddit/coments/id=32rc86/",
+                     headers=headers); pp.pprint(response.json())
+
+
+# -------------------------------------------------------------------- #
+
+# Auth
+reddit = praw.Reddit(client_id=Client_ID,
+                 client_secret=Client_secret,
+                 password=Password,
+                 user_agent='DataExtraction by /u/lonely_dingleberry',
+                 username='Username')
+
+# Others
+submissions = reddit.get_subreddit('opensource').get_hot(limit=5)
+
+[str(x) for x in submissions]
+
+submission = reddit.submission(url='https://www.reddit.com/r/funny/comments/3g1jfi/buttons/')
 
 
